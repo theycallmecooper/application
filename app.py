0e6ba69fb30e4a61569b613d2d42ebb6dd0a69e5
cthomson <cthomson@student.saintaug.nsw.edu.au>
2024-11-11 09:29:50 +1100

    5

--- app.py
+++ app.py
@@ -1,41 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
+    return render_template('index.html')
 
-    html = '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Homepage</title>
-    </head>
-    <body>
-        <h1>Welcome to the home page</h1>
-        <p>You can go and read about us on our <a href='./about'>about</a> page!</p>
-    </body>
-    </html>
-    '''
-
-    return html
-
-@app.route('/about')
-def about():
-
-    html = '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>About</title>
-    </head>
-    <body>
-        <h1>About page</h1>
-        <p>This is the about page. To return to the home page click <a href='.'>here</a>!</p>
-    </body>
-    </html>
-    '''
-
-    return html
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 app.run(debug=True, reloader_type='stat', port=5000)
--- insert_data.py
+++ insert_data.py
@@ -9,16 +9,14 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 #Insert users with hashed passwords
-user1 = User(username='john_doe')
-password = generate_password_hash('password123')
-user2 = User(username='jane_doe')
-password = generate_password_hash('password123')
+user1 = User(username='john_doe', password=generate_password_hash('password123'))
+user2 = User(username='jane_doe', password=generate_password_hash('password123'))
 
 session.add(user1)
 session.add(user2)
 session.commit
 
-#Insert tasks
+session.commit()
 task1 = ToDo(task='Learn SQLAlchemy', done=False, user_id=user1.id)
 task2 = ToDo(task='Build an app', done=False, user_id=user2.id)
 
