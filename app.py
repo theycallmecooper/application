c7f144b6a3161050811a43b76d53fae0b582e3df
cthomson <cthomson@student.saintaug.nsw.edu.au>
2024-11-18 09:24:43 +1100

    buttons work

--- app.py
+++ app.py
@@ -37,6 +37,12 @@ def login():
     
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     @app.route('/signup', methods=['GET', 'POST'])
